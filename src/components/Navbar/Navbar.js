888b77ee005807b9528aa05b73206e6d7cb4fd05
Ben Gelinas <benjamenjalinas@gmail.com>
2024-02-05 11:14:03 -0500

    Mobile device Navbar rework

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
-import {Button, Nav, Navbar, NavbarBrand, NavItem, NavLink} from "reactstrap";
+import {Button, Dropdown, Nav, Navbar, NavbarBrand, NavItem, NavLink} from "reactstrap";
 import {useEffect, useState} from "react";
 
 function MyNavbar() {
     const [width, setWidth] = useState(window.innerWidth)
+    const [dropdownOpen, setDropdownOpen] = useState(false)
     useEffect(() => {
         function handleResize() {
             const newwidth = window.innerWidth
@@ -51,12 +52,31 @@ function MyNavbar() {
                 fontSize: '1rem',
                 alignItems: "center",
             }}>
-                <Button style={{background: '#000000', border: '0px'}} onClick={() => console.log("hi")}>
+                <Button style={{background: '#000000', border: '0px'}} onClick={() => setDropdownOpen(!dropdownOpen)}>
                     <i className={'bi bi-list'}></i>
                 </Button>
+                {dropdownOpen && <Dropdown className={"small-dropdown"}>
+                    <NavbarDropdownItem page={'/'}>
+                        Home
+                    </NavbarDropdownItem>
+                    <NavbarDropdownItem page={'/about'}>
+                        About
+                    </NavbarDropdownItem>
+                    <NavbarDropdownItem page={'/projects'}>
+                        Projects
+                    </NavbarDropdownItem>
+                </Dropdown>}
             </div>}
 
         </>
+    )
+}
+
+function NavbarDropdownItem(props) {
+    return (
+        <div className={'navbar-dropdown-item'} onClick={() => window.location.href = props.page}>
+            {props.children}
+        </div>
     )
 }
 
