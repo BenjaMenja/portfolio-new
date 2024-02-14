ab125064ebab5dc270d3ce9457078bc959c24361
Ben Gelinas <benjamenjalinas@gmail.com>
2024-02-14 00:20:18 -0500

    Mobile device about rework

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -1,16 +1,9 @@
-import {Col, Row} from "reactstrap";
+
 
 function FooterBar() {
     return (
-        <div style={{marginTop: '3rem', color: '#FFFFFF', fontSize: '1.1rem'}}>
-            <Row style={{
-                background: '#000000',
-                maxWidth: '100vw',
-                height: '5vh',
-                alignItems: 'center'
-            }}>
-                <Col>This website was built by Ben Gelinas</Col>
-            </Row>
+        <div className={"footer-bar"}>
+            This website was built by Ben Gelinas
         </div>
     )
 }
